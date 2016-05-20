2c2f763b00dc26c836290002dda93fe6c8efe61d
LMRemy <remy.le-mouillour@isep.fr>
2016-05-20 11:22:36 +0200

    Statut "ANCIEN" ds BDD

--- wacajou/src/main/java/com/wacajou/data/jpa/service/UserService.java
+++ wacajou/src/main/java/com/wacajou/data/jpa/service/UserService.java
@@ -7,6 +7,6 @@ import com.wacajou.data.jpa.domain.User;
 public interface UserService {
 	void Create (String login, String promo, String statut) throws ServiceException;
 	User Consult (String fname, String lname, String login, String mail) throws ServiceException;
-//	void Update (String ) throws ServiceException;
+	void Update (String fname, String lname, String mail) throws ServiceException;
 
 }
--- wacajou/src/main/java/com/wacajou/data/jpa/service/impl/UserServiceImpl.java
+++ wacajou/src/main/java/com/wacajou/data/jpa/service/impl/UserServiceImpl.java
@@ -69,10 +69,10 @@ public class UserServiceImpl implements UserService {
 		return null;
 	}
 
-	/*@Override
-	public void Update() {
+	@Override
+	public void Update(String fname, String lname, String mail) {
 		// TODO Auto-generated method stub
 		
-	}*/
+	}
 
 }
